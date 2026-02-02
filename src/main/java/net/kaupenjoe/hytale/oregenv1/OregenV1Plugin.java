bcdd9b6d350fe18414b5b68b0c2517561497f4c2
HaRo0 <34543900+HaRo0@users.noreply.github.com>
2026-02-02 21:29:52 +0100

    Added Asset Editor capability

--- src/main/java/net/kaupenjoe/hytale/oregenv1/OregenV1Plugin.java
+++ src/main/java/net/kaupenjoe/hytale/oregenv1/OregenV1Plugin.java
@@ -1,11 +1,20 @@
 package net.kaupenjoe.hytale.oregenv1;
 
+import com.hypixel.hytale.assetstore.AssetUpdateQuery;
+import com.hypixel.hytale.builtin.asseteditor.AssetEditorPlugin;
+import com.hypixel.hytale.builtin.asseteditor.AssetPath;
+import com.hypixel.hytale.builtin.asseteditor.EditorClient;
+import com.hypixel.hytale.builtin.asseteditor.assettypehandler.AssetTypeHandler;
 import com.hypixel.hytale.codec.lookup.Priority;
+import com.hypixel.hytale.protocol.packets.asseteditor.AssetEditorAssetType;
+import com.hypixel.hytale.protocol.packets.asseteditor.AssetEditorEditorType;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import com.hypixel.hytale.server.core.universe.world.worldgen.provider.IWorldGenProvider;
 import net.kaupenjoe.hytale.oregenv1.providers.KaupenWorldGenProvider;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
+
+import java.nio.file.Path;
 
 public class OregenV1Plugin extends JavaPlugin {
 
@@ -20,5 +29,41 @@ public class OregenV1Plugin extends JavaPlugin {
         var defaultProvider = IWorldGenProvider.CODEC.getClassFor("Hytale");
         IWorldGenProvider.CODEC.remove(defaultProvider);
         IWorldGenProvider.CODEC.register(Priority.DEFAULT.before(1), "Hytale", KaupenWorldGenProvider.class, KaupenWorldGenProvider.CODEC);
+
+        AssetEditorPlugin.get().getAssetTypeRegistry().registerAssetType(new TypeHandler("CaveModifications", "Server/World/KaupenOres/CaveModifications"));
+        AssetEditorPlugin.get().getAssetTypeRegistry().registerAssetType(new TypeHandler("ZoneData", "Server/World/KaupenOres/Ores"));
+
+    }
+
+    private static class TypeHandler extends AssetTypeHandler {
+
+        protected TypeHandler(String id, String path) {
+
+            super(new AssetEditorAssetType(id, null, false, path, ".json", AssetEditorEditorType.JsonSource));
+        }
+
+        @Override
+        public AssetLoadResult loadAsset(AssetPath var1, Path var2, byte[] var3, AssetUpdateQuery var4, EditorClient var5) {
+
+            return AssetLoadResult.ASSETS_CHANGED;
+        }
+
+        @Override
+        public AssetLoadResult unloadAsset(AssetPath var1, AssetUpdateQuery var2) {
+
+            return AssetLoadResult.ASSETS_CHANGED;
+        }
+
+        @Override
+        public AssetLoadResult restoreOriginalAsset(AssetPath var1, AssetUpdateQuery var2) {
+
+            return AssetLoadResult.ASSETS_CHANGED;
+        }
+
+        @Override
+        public AssetUpdateQuery getDefaultUpdateQuery() {
+
+            return AssetUpdateQuery.DEFAULT_NO_REBUILD;
+        }
     }
 }
